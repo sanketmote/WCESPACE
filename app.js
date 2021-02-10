b199263b977570bd33c81557cad6e9d20b0e86e6
Sanket Mote <sanketmote01@gmail.com>
2021-02-10 18:59:11 +0530

    Adding Evrything - Home page and node module

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const bodyparser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("home")
+}); 
+
+// app.post("/", function(req,res){
+//     var item = req.body.newItem;
+//     items.push(item);
+//     res.redirect("/")
+//     console.log(item);
+// })
+
+app.listen(3000 , function(){
+    console.log("Server Started on port 3000");
+})
