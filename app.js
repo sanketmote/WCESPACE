0a5cd3a1f37635284dff63284a3dcb9665f9df3e
pavan7494 <pavanshinde7494@gmail.com>
2021-03-03 18:34:52 +0530

    Added gitignore , procfile , Updated ports

--- app.js
+++ app.js
@@ -1226,8 +1226,13 @@ app.get('/shelf',(req,res)=>{
 // app.listen(3000,()=>{
 //     console.log("server is running on port 3000");
 // });
-  
-app.listen(3000, function(){
-    console.log("server is running on port 3000");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("server is running successfully");
   })
   
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1330,6 +1330,12 @@ app.get('/shelf', function (req, res) {
 //     console.log("server is running on port 3000");
 // });
 
-app.listen(3000, function () {
-  console.log("server is running on port 3000");
+var port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("server is running successfully");
 });
